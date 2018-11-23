84467f417ba8a4648f225ccaaa807e55cc8b8bf8
Sujin Park <sujinpark1031@gmail.com>
2018-11-23 08:20:26 -0800

    Add callbacks_and_functions parts 1 thru 3

---
+++ W5D5/callbacks_and_functions_i.js
@@ -0,0 +1,10 @@
+window.setTimeout(function () {
+  alert('HAMMER TIME!');
+}, 5000);
+
+function hammerTime (time) {
+  window.setTimeout(function () {
+    alert(`${time} is hammertime!`);
+  }, time);
+}
+
