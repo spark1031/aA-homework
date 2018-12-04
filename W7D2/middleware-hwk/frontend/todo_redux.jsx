3ada60a27cb21f605484635d1de52490f5bd982e
Sujin Park <sujinpark1031@gmail.com>
2018-12-03 21:01:40 -0800

    Add completed middleware hwk

---
+++ W7D2/middleware-hwk/frontend/todo_redux.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import configureStore from "./store/store";
+
+import Root from "./components/root";
+
+//transforms dispatch into a more complicated dispatch
+// const applyMiddlewares = (store, middlewares) => {
+// 	let dispatch = store.dispatch;
+// 	middlewares.forEach(middleware => {
+// 		dispatch = middleware(store)(dispatch);
+// 	});
+// 	return Object.assign({}, store, { dispatch });
+// };
+
+document.addEventListener("DOMContentLoaded", () => {
+	const preloadedState = localStorage.state
+		? JSON.parse(localStorage.state)
+		: {};
+	let store = configureStore(preloadedState);
+	// store.dispatch = addLoggingToDispatch(store);
+	// store = applyMiddlewares(store, [addLoggingToDispatch]);
+	const root = document.getElementById("content");
+	ReactDOM.render(<Root store={store} />, root);
+});
